e389af479da92f06c0c3362d4e48d3a560115052
Jeferson Farias Calazans <calazans10@gmail.com>
2017-04-12 23:05:33 -0300

    Add more integration tests

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -10,23 +10,17 @@ describe('Routes: Products', () => {
       });
   });
 
+  const defaultId = '56cb91bdc3464f14678934ca';
   const defaultProduct = {
     name: 'Default Product',
     description: 'product description',
     price: 100
   };
-
-  const expectedProduct = {
-    __v: 0,
-    _id: '56cb91bdc3464f14678934ca',
-    name: 'Default Product',
-    description: 'product description',
-    price: 100
-  };
+  const expectedProduct = Object.assign({}, {  __v: 0, _id: defaultId }, defaultProduct);
 
   beforeEach(() => {
     const product = new Product(defaultProduct);
-    product._id = '56cb91bdc3464f14678934ca';
+    product._id = defaultId;
     return Product.remove({})
       .then(() => product.save());
   });
@@ -39,16 +33,60 @@ describe('Routes: Products', () => {
         .get('/products')
         .end((err, res) => {
           expect(res.body).to.eql([expectedProduct]);
-          done(err)
+          done(err);
         });
     });
   });
 
-  describe('POST /products', () => {});
+  describe('POST /products', () => {
+    it('should create a product into the database', done => {
+      const customId = '56cb91bdc3464f14678934ba';
+      const newProduct = Object.assign({},{ _id: customId }, defaultProduct);
+      const expectedSavedProduct = Object.assign({},{ __v: 0 }, newProduct);
 
-  describe('GET /products/:id', () => {});
+      request
+        .post('/products')
+        .send(newProduct)
+        .end((err, res) => {
+          expect(res.body).to.eql(expectedSavedProduct);
+          done(err);
+        });
+    });
+  });
 
-  describe('PUT /products/:id', () => {});
+  describe('GET /products/:id', () => {
+    it('should return one product', done => {
+      request
+        .get(`/products/${defaultId}`)
+        .end((err, res) => {
+          expect(res.body).to.eql(expectedProduct);
+          done(err);
+        });
+    });
+  });
 
-  describe('DELETE /products/:id', () => {});
+  describe('PUT /products/:id', () => {
+    it('should update a product into the database', done => {
+      const updatedProduct = Object.assign({}, { name: 'Custom name' }, expectedProduct);
+
+      request
+        .put(`/products/${defaultId}`)
+        .send(updatedProduct)
+        .end((err, res) => {
+          expect(res.body).to.eql(updatedProduct);
+          done(err);
+        });
+    });
+  });
+
+  describe('DELETE /products/:id', () => {
+    it('should delete a product from database', done => {
+      request
+        .delete(`/products/${defaultId}`)
+        .end((err, res) => {
+          expect(res.status).to.eql(204);
+          done(err);
+        });
+    });
+  });
 });
