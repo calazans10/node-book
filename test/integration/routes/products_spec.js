befc2e939f99dde4c4c5dbcde42302ceeaf1f838
Jeferson Farias Calazans <calazans10@gmail.com>
2017-04-12 22:41:12 -0300

    Rename folder name

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -2,20 +2,20 @@ import Product from '../../../src/models/product';
 
 describe('Routes: Products', () => {
   let request;
-  
+
   before(() => {
     return setupApp()
       .then(app => {
         request = supertest(app)
       });
   });
-  
+
   const defaultProduct = {
     name: 'Default Product',
     description: 'product description',
     price: 100
   };
-  
+
   const expectedProduct = {
     __v: 0,
     _id: '56cb91bdc3464f14678934ca',
@@ -23,16 +23,16 @@ describe('Routes: Products', () => {
     description: 'product description',
     price: 100
   };
-  
+
   beforeEach(() => {
     const product = new Product(defaultProduct);
     product._id = '56cb91bdc3464f14678934ca';
     return Product.remove({})
       .then(() => product.save());
   });
-  
+
   afterEach(() => Product.remove({}));
-  
+
   describe('GET /products', () => {
     it('should return a list of products', done => {
       request
@@ -43,4 +43,12 @@ describe('Routes: Products', () => {
         });
     });
   });
+
+  describe('POST /products', () => {});
+
+  describe('GET /products/:id', () => {});
+
+  describe('PUT /products/:id', () => {});
+
+  describe('DELETE /products/:id', () => {});
 });
--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -17,12 +17,12 @@ describe('Constrollers: Products', () => {
       const response = {
         send: sinon.spy()
       };
-      
+
       Product.find = sinon.stub();
       Product.find.withArgs({}).resolves(defaultProduct);
 
       const productsController = new ProductsController(Product);
-      
+
       return productsController.index(defaultRequest, response)
         .then(() => {
           sinon.assert.calledWith(response.send, defaultProduct);
@@ -40,12 +40,12 @@ describe('Constrollers: Products', () => {
       Product.find.withArgs({}).rejects(new Error('Error'));
 
       const productsController = new ProductsController(Product);
-      
+
       return productsController.index(defaultRequest, response)
-      .then(() => {
-        sinon.assert.calledWith(response.send, 'Error');
-        sinon.assert.calledWith(response.status, 400);
-      });
+        .then(() => {
+          sinon.assert.calledWith(response.send, 'Error');
+          sinon.assert.calledWith(response.status, 400);
+        });
     });
   });
 
@@ -56,38 +56,38 @@ describe('Constrollers: Products', () => {
         send: sinon.spy(),
         status: sinon.stub()
       };
-      
+
       class fakeProduct {
         save() {}
       }
-      
+
       response.status.withArgs(201).returns(response);
       sinon.stub(fakeProduct.prototype, 'save').withArgs().resolves();
-      
+
       const productsController = new ProductsController(fakeProduct);
-      
+
       return productsController.create(requestWithBody, response)
         .then(() => {
           sinon.assert.calledWith(response.send);
           sinon.assert.calledWith(response.status, 201);
         });
     });
-    
+
     it('should return 412 when an error occurs on creating a product', () => {
       const response = {
         send: sinon.spy(),
         status: sinon.stub()
       };
-      
+
       class fakeProduct {
         save() {}
       }
-      
+
       response.status.withArgs(412).returns(response);
       sinon.stub(fakeProduct.prototype, 'save').withArgs().rejects(new Error('Error'));
-      
+
       const productsController = new ProductsController(fakeProduct);
-      
+
       return productsController.create(defaultRequest, response)
         .then(() => {
           sinon.assert.calledWith(response.send, 'Error');
@@ -95,7 +95,7 @@ describe('Constrollers: Products', () => {
         });
     });
   });
-  
+
   describe('show() product', () => {
     it('should call send with one product', () => {
       const fakeId = 'a-fake-id';
@@ -107,7 +107,7 @@ describe('Constrollers: Products', () => {
       const response = {
         send: sinon.spy()
       };
-      
+
       Product.findById = sinon.stub();
       Product.findById.withArgs(fakeId).resolves(defaultProduct[0]);
 
@@ -118,7 +118,7 @@ describe('Constrollers: Products', () => {
           sinon.assert.calledWith(response.send, defaultProduct[0]);
         });
     });
-    
+
     it('should return 404 when an error occurs', () => {
         const fakeId = 'a-fake-id';
         const request = {
@@ -130,13 +130,13 @@ describe('Constrollers: Products', () => {
           send: sinon.spy(),
           status: sinon.stub()
         };
-        
+
         response.status.withArgs(404).returns(response);
         Product.findById = sinon.stub();
         Product.findById.withArgs(fakeId).rejects(new Error('Error'));
-        
+
         const productsController = new ProductsController(Product);
-        
+
         return productsController.show(request, response)
           .then(() => {
             sinon.assert.calledWith(response.send, 'Error');
@@ -144,7 +144,7 @@ describe('Constrollers: Products', () => {
           });
     });
   });
-  
+
   describe('update() product', () => {
     it('should repond successfully when updating a product', () => {
       const fakeId = 'a-fake-id';
@@ -163,15 +163,15 @@ describe('Constrollers: Products', () => {
       const response = {
         send: sinon.spy()
       };
-      
+
       class fakeProduct {
         static findOneAndUpdate() {}
       }
-      
+
       sinon.stub(fakeProduct, 'findOneAndUpdate').
         withArgs({ _id: fakeId }, updatedProduct).
         resolves(updatedProduct);
-        
+
       const productsController = new ProductsController(fakeProduct);
 
       return productsController.update(request, response)
@@ -179,7 +179,7 @@ describe('Constrollers: Products', () => {
           sinon.assert.calledWith(response.send, updatedProduct);
         });
     });
-    
+
     it('should return 412 when an error occurs on updating a product', () => {
       const fakeId = 'a-fake-id';
       const updatedProduct = {
@@ -198,16 +198,16 @@ describe('Constrollers: Products', () => {
         send: sinon.spy(),
         status: sinon.stub()
       };
-      
+
       class fakeProduct {
         static findOneAndUpdate() {}
       }
-      
+
       response.status.withArgs(412).returns(response);
       sinon.stub(fakeProduct, 'findOneAndUpdate').
         withArgs({ _id: fakeId }, updatedProduct).
         rejects(new Error('Error'));
-        
+
       const productsController = new ProductsController(fakeProduct);
 
       return productsController.update(request, response)
@@ -217,7 +217,7 @@ describe('Constrollers: Products', () => {
         });
     });
   });
-  
+
   describe('destroy() product', () => {
     it('should repond successfully when deleting a product', () => {
       const fakeId = 'a-fake-id';
@@ -233,9 +233,9 @@ describe('Constrollers: Products', () => {
       class fakeProduct {
         static remove() {}
       }
-      
+
       sinon.stub(fakeProduct, 'remove').withArgs({ _id: fakeId }).resolves([1]);
-      
+
       const productsController = new ProductsController(fakeProduct);
 
       return productsController.destroy(request, response)
@@ -243,7 +243,7 @@ describe('Constrollers: Products', () => {
           sinon.assert.calledWith(response.sendStatus, 204);
         });
     });
-    
+
     it('should return 412 when an error occurs on deleting a product', () => {
       const fakeId = 'a-fake-id';
       const request = {
@@ -259,12 +259,12 @@ describe('Constrollers: Products', () => {
       class fakeProduct {
         static remove() {}
       }
-      
+
       response.status.withArgs(400).returns(response);
       sinon.stub(fakeProduct, 'remove').withArgs({ _id: fakeId }).rejects(new Error('Error'));
-      
+
       const productsController = new ProductsController(fakeProduct);
-      
+
       return productsController.destroy(request, response)
         .then(() => {
           sinon.assert.calledWith(response.send, 'Error');
